2cfd87108bb981688f0c6b566bd84922548be85f
Adam Snyder <adam296@live.com>
2019-05-13 20:39:09 -0500

    Refactored animation script into playermovement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,18 +6,35 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
 
+    private Animator anim;
+
     float horizontalMove = 0f;
     public float runSpeed = 40;
     bool jump = false;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
-        if(Input.GetButtonDown("Jump"))
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+
+        if (Input.GetButtonDown("Jump"))
         {
             jump = true;
+            anim.SetTrigger("jump");
         }
         
     }
