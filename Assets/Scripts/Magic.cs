ee745b58a3a0c4e131e7b1c30c399526d0872c4d
Adam Snyder <adam296@live.com>
2019-05-15 13:57:12 -0500

    Added moving fireball

---
+++ Assets/Scripts/Magic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magic : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject fireballPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+        
+    }
+
+    void Shoot()
+    {
+        // shooting logic
+        Instantiate(fireballPrefab, firePoint.position, firePoint.rotation);
+    }
+}
